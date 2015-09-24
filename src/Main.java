0f7e99cd50be4334fa8d66ff246dffc497f8c3d0
Weiping HUANG <Nightonke@outlook.com>
2015-09-24 15:42:07 +0800

    Inorder Successor in BST

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,21 @@ public class Main {
 	public static void main(String[] args){
 		Solution solution = new Solution();
 		
+		TreeNode root = new TreeNode(2);
+		TreeNode rootLeft = new TreeNode(1);
+		TreeNode rootRight = new TreeNode(3);
+		root.left = rootLeft;
+		root.right = rootRight;
+		TreeNode p = new TreeNode(3);
+		
+		TreeNode ans = solution.inorderSuccessor(root, p);
+		
+		if (ans == null) {
+			System.out.println("isNULL");
+		} else {
+			System.out.println(ans.val);
+		}
+		
 	}
 	
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,37 @@
-
 public class Solution {
-    
+	
+//	Given a binary search tree and a node in it, find the in-order successor of
+//	that node in the BST.
+//	Note: If the given node has no in-order successor in the tree, return null.
+	
+	private int targetVal;
+	
+	public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        
+		targetVal = p.val;
+		return inOrderTravel(root);
+		
+    }
+	
+	public TreeNode inOrderTravel(TreeNode root) {
+		// travel the whole tree in-order
+		if (root == null) {
+			return root;
+		}
+		if (targetVal >= root.val) {
+			// if the current node's value is bigger than the target
+			// the targetNode must be in the right side
+			return inOrderTravel(root.right);
+		} else {
+			// otherwise the targetNode may be in the left side
+			// and also, the targetNode may be the current node
+			TreeNode targetTreeNodeInLeft = inOrderTravel(root.left);
+			if (targetTreeNodeInLeft == null) {
+				return root;
+			} else {
+				return targetTreeNodeInLeft;
+			}
+		}
+	}
+	
 }
