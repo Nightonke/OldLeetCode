565b2bd82155828357d615e57ad982e5fd9ddded
Weiping HUANG <Nightonke@outlook.com>
2015-10-26 23:37:21 +0800

    Update to No.238

---
+++ src/_0238_Product_of_Array_Except_Self.java
@@ -0,0 +1,35 @@
+
+public class _0238_Product_of_Array_Except_Self {
+
+/*	Given an array of n integers where n > 1, nums, return an array output such 
+ *  that output[i] is equal to the product of all the elements of nums except 
+ *  nums[i].
+
+	Solve it without division and in O(n).
+
+	For example, given [1,2,3,4], return [24,12,8,6].
+
+	Follow up:
+	Could you solve it with constant space complexity? (Note: The output array 
+	does not count as extra space for the purpose of space complexity analysis.)
+	*/
+	
+	class Solution {
+	public:
+	    vector<int> productExceptSelf(vector<int>& nums) {
+	        vector<int> ans(nums.size());
+	        int p = 1;
+	        for (int i = 0; i < nums.size(); i++) {
+	            ans[i] = p;
+	            p *= nums[i];
+	        }
+	        p = 1;
+	        for (int i = nums.size() - 1; i >= 0; i--) {
+	            ans[i] *= p;
+	            p *= nums[i];
+	        }
+	        return ans;
+	    }
+	};
+	
+}
